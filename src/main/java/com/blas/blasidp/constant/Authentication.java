9fa19eccec2ff708328f9b74918ff72e9ee905cf
DatNguyen <vinhdat4999@gmail.com>
2022-07-05 22:52:26 +0700

    #comment: release v1.2.1

--- src/main/java/com/blas/blasidp/constant/Authentication.java
+++ src/main/java/com/blas/blasidp/constant/Authentication.java
@@ -6,6 +6,9 @@ public class Authentication {
     public static final String ACCOUNT_INACTIVE = "ACCOUNT INACTIVE";
     public static final String ACCOUNT_BLOCKED = "ACCOUNT BLOCKED";
     public static final String REGISTER_SUCCESSFULLY = "REGISTER SUCCESSFULLY";
+    public static final String VERIFY_SUCCESSFULLY = "VERIFY SUCCESSFULLY";
+    public static final String SENT = "SENT";
+    public static final String VERIFY_FAILED = "VERIFY FAILED";
     public static final int THRESHOLD_BLOCK_ACCOUNT = 5;
 
 }
