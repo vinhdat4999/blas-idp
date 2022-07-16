c17542c37ed1c872982db8e9ed5897b97bfe8c80
DatNguyen <vinhdat4999@gmail.com>
2022-07-16 11:51:20 +0700

    #comment: aplly github action

---
+++ src/test/java/com/blas/blasidp/controller/ControllerTest.java
@@ -0,0 +1,15 @@
+package com.blas.blasidp.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ControllerTest {
+
+
+    @Test
+    public void testGetRoleById() {
+
+        assertEquals(true, true);
+    }
+}
