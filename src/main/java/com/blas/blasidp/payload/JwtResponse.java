e4131ce3767b2b8dc54cd10d77b23ab221ab5538
Nguyen Vinh Dat <39378896+vinhdat4999@users.noreply.github.com>
2022-09-01 15:33:27 +0700

    #comment: update version v1.5.0 (#15)

--- src/main/java/com/blas/blasidp/payload/JwtResponse.java
+++ src/main/java/com/blas/blasidp/payload/JwtResponse.java
@@ -1,9 +1,15 @@
 package com.blas.blasidp.payload;
 
+import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
 public class JwtResponse {
 
   private final String jwtToken;
+  private final long expireIn;
+  private final LocalDateTime timeExpired;
+  private final String refreshToken;
+  private final String identityToken;
+  private final String tokenType;
 }
