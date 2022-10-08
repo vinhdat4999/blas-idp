d1187716cf26351d1b4023dac17376bf592f0e65
DatNguyen <vinhdat4999@gmail.com>
2022-10-08 22:55:47 +0700

    #comment: v1.7.0

--- src/main/java/com/blas/blasidp/configuration/WebSecurityConfig.java
+++ src/main/java/com/blas/blasidp/configuration/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.blas.blasidp.configuration;
 
 import com.blas.blascommon.security.hash.Sha256Encoder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,16 +8,15 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-  @Autowired
-  UserDetailsService jwtUserDetailsService;
+public class WebSecurityConfig {
 
   @Bean
   public Sha256Encoder passwordEncoder() {
@@ -26,20 +24,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   }
 
   @Bean
-  @Override
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
+  public AuthenticationManager authenticationManager(HttpSecurity http,
+      UserDetailsService jwtUserDetailsService) throws Exception {
+    AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(
+        AuthenticationManagerBuilder.class);
+    authenticationManagerBuilder.userDetailsService(jwtUserDetailsService)
+        .passwordEncoder(passwordEncoder());
+    return authenticationManagerBuilder.build();
   }
 
-  @Override
-  public void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
-  }
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf().disable();
-    http.authorizeRequests()
-        .antMatchers("/auth/**").permitAll();
+    http.authorizeRequests().antMatchers("/auth/**").permitAll();
+    http.headers().frameOptions().sameOrigin();
+    return http.build();
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+      }
+    };
   }
 }
